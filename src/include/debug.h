d0887ded66e395846aa9b8afb06a0dae92f31279
Jörg Thalheim <joerg@higgsboson.tk>
2015-05-16 20:53:26 +0200

    debug.h: fix macro redefinition compiler warning

--- src/include/debug.h
+++ src/include/debug.h
@@ -29,6 +29,7 @@
 #include <assert.h>
 
 #ifdef DEBUG
+    #undef DEBUG
     #define DEBUG(...) fprintf(stderr, "%20s:%-4d: ", __FILE__, __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #else
     #define DEBUG(_fmt, ...)
