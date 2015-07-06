3b86ca00ad7b51d73808929687bb165aa71ba560
Jörg Thalheim <joerg@higgsboson.tk>
2015-07-06 10:07:09 +0200

    set thunkStart to _start symbol on initialisation

--- src/source/tthread/logentry.cpp
+++ src/source/tthread/logentry.cpp
@@ -1,10 +1,16 @@
 #include "tthread/logentry.h"
 #include "xthread.h"
 
+extern char _start;
+
 namespace tthread {
 void logentry::setThread(xthread thread) {
   threadId = thread.getId();
   thunkId = thread.getThunkId();
   thunkStart = thread.getThunkStart();
+
+  if (thunkStart == NULL) {
+    thunkStart = (const void *)&_start;
+  }
 }
 }
