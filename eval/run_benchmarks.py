de78b5f45b79236f12285fdaee9d911da1023276
Jörg Thalheim <joerg@higgsboson.tk>
2015-09-16 09:29:09 +0200

    benchmark: fix enable/disable cpu; catch benchmark errors

--- eval/run_benchmarks.py
+++ eval/run_benchmarks.py
@@ -4,7 +4,7 @@ import os, sys
 import subprocess
 import time
 import argparse
-import multiprocessing
+import glob, threading
 
 if sys.version_info >= (3,3):
     import shlex
@@ -13,20 +13,23 @@ else:
     import pipes
     quote = pipes.quote
 
-SCRIPT_ROOT=os.path.dirname(os.path.realpath(__file__))
-
 def sh(cmd, verbose=True):
     if verbose:
         args = ' '.join(map(lambda s: quote(s), cmd[1:]))
         sys.stderr.write("$ %s %s\n" % (cmd[0], args))
     return subprocess.call(cmd)
 
-TOTAL_THREADS = multiprocessing.count()
+SCRIPT_ROOT = os.path.dirname(os.path.realpath(__file__))
+TOTAL_THREADS = len(glob.glob("/sys/devices/system/cpu/cpu*"))
+
 def set_cpus(threads=TOTAL_THREADS, verbose=True):
-    for i in list(range(1, TOTAL_THREADS)):
-        enable = int((i % (TOTAL_THREADS / threads)) == 0)
+    for i in list(range(1, TOTAL_THREADS - 1)):
+        enable = (i % (TOTAL_THREADS / threads)) == 0
         with open("/sys/devices/system/cpu/cpu%d/online" % i, "w") as f:
-            f.write(str(enable))
+            if enable:
+                f.write("1")
+            else:
+                f.write("0")
 
 BENCHMARKS = [
         "kmeans",
@@ -63,16 +66,19 @@ def main():
     args = parser.parse_args()
 
     os.chdir(os.path.join(SCRIPT_ROOT, ".."))
-    for bench in BENCHMARKS:
-        path = os.path.join(args.output, bench + ".csv")
-        with open(path, "a+") as f:
-            sys.stderr.write(">> run %s\n" % bench)
-            for threads in [16,8,4,2]:
-                sh(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-DNCORES=%d" % threads, "-DBENCHMARK=On"])
-                mean1 = run_benchmark("bench-%s-pthread" % bench, threads)
-                mean2 = run_benchmark("bench-%s-tthread" % bench, threads)
-                f.write("%d;%f;%f\n" %(threads, mean1, mean2))
-                f.flush()
+    for threads in [16,8,4,2]:
+        for bench in BENCHMARKS:
+            try:
+                path = os.path.join(args.output, bench + ".csv")
+                with open(path, "a+") as f:
+                    sys.stderr.write(">> run %s\n" % bench)
+                    sh(["cmake", "-DCMAKE_BUILD_TYPE=Release", "-DNCORES=%d" % threads, "-DBENCHMARK=On"])
+                    mean1 = run_benchmark("bench-%s-pthread" % bench, threads)
+                    mean2 = run_benchmark("bench-%s-tthread" % bench, threads)
+                    f.write("%d;%f;%f\n" %(threads, mean1, mean2))
+                    f.flush()
+            except OSError as e:
+                print("failed to run %s: %s" %(bench,e))
 
 if __name__ == '__main__':
     main()
