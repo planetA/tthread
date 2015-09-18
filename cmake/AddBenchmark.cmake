ae7081cc05653a4e7f9f444a04d35416ac3d39b2
Jörg Thalheim <joerg@higgsboson.tk>
2015-09-18 10:57:34 +0200

    eval/cmake: fix target name

--- cmake/AddBenchmark.cmake
+++ cmake/AddBenchmark.cmake
@@ -12,8 +12,8 @@ macro(AddBenchmark benchmark)
   add_custom_target(bench-${benchmark}-pthread
     COMMAND ${CMAKE_COMMAND} -E
     time ./${benchmark}-pthread ${AddBenchmark_ARGS})
-  target_include_directories(bench-${benchmark}-pthread PRIVATE include . ${AddBenchmark_INCLUDES})
-  target_compile_definitions(bench-${benchmark}-pthread ${AddBenchmark_DEFINITIONS})
+  target_include_directories(${benchmark}-pthread PRIVATE include . ${AddBenchmark_INCLUDES})
+  target_compile_definitions(${benchmark}-pthread PRIVATE ${AddBenchmark_DEFINITIONS})
 
   add_executable(${benchmark}-tthread ${AddBenchmark_FILES})
   target_link_libraries(${benchmark}-tthread LINK_PUBLIC tthread
@@ -21,6 +21,6 @@ macro(AddBenchmark benchmark)
   add_custom_target(bench-${benchmark}-tthread
     COMMAND ${CMAKE_COMMAND} -E
     time ./${benchmark}-tthread ${AddBenchmark_ARGS})
-  target_include_directories(bench-${benchmark}-tthread PRIVATE include . ${AddBenchmark_INCLUDES})
-  target_compile_definitions(bench-${benchmark}-tthread ${AddBenchmark_DEFINITIONS})
+  target_include_directories(${benchmark}-tthread PRIVATE include . ${AddBenchmark_INCLUDES})
+  target_compile_definitions(${benchmark}-tthread PRIVATE ${AddBenchmark_DEFINITIONS})
 endmacro(AddBenchmark)
